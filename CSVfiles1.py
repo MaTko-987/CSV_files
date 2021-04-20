75f419254eeffa9e3748712c046d790b98798a73
MaTko-987 <kwikzzstep@gmail.com>
2021-04-20 12:12:29 +0200

    Second commit

--- CSVfiles1.py
+++ CSVfiles1.py
@@ -3,7 +3,7 @@ import csv
 
 
 with open("data.csv",) as csv_file:
-    csv_lines = csv_file.split()
+    csv_lines = csv_file.read().split()
     print(csv_lines)
 
     for line in csv_lines:
@@ -12,10 +12,5 @@ with open("data.csv",) as csv_file:
 
 
 
-with open("ninja.txt", "r") as ninja_file:
-    ninja_lines = ninja_file.read().splitlines() #lanac funkcija, čitanje iz fajla i splitaju se linije u isto vrijeme
-    print(ninja_lines)
 
-    for line in ninja_lines: #line je linija unutar ninja.txt --- for petlja prolazi kroz cijeli file i u svakom krugu će biti jedna linija file --- line ili x ili item
-        print(line)
 
